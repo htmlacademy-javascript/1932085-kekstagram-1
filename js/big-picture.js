fe64939ff3b60013f4397016d552c4afaa1eac38
Irina Vinogorskaya <bigshprot@yahoo.com>
2024-08-12 22:15:15 +0300

    сдача многострадального ДЗ по разделу 8

--- js/big-picture.js
+++ js/big-picture.js
@@ -7,6 +7,10 @@ const socialCommentsCount = document.querySelector('.social__comment-count');
 const newCommentsLoader = document.querySelector('.comments-loader');
 const socialCommentClone = document.querySelector('.social__comment');
 const commentsContainer = document.querySelector('.social__comments');
+const COMMENTS_PORTION = 5;
+const localComments = [];
+let total = 0;
+let renderedCommentsCount = 0;
 
 const createSocialCommentTag = ({ avatar, message, name, id }) => {
   const socialComment = socialCommentClone.cloneNode(true);
@@ -17,30 +21,49 @@ const createSocialCommentTag = ({ avatar, message, name, id }) => {
   return socialComment;
 };
 
-const createComments = (comments) => {
+const renderStatistics = () => {
+  socialCommentsCount.innerHTML = `${renderedCommentsCount} из <span class="comments-count">${total}</span> комментариев`;
+};
+
+const renderLoader = () => {
+  if (localComments.length) {
+    newCommentsLoader.classList.remove('hidden');
+  } else {
+    newCommentsLoader.classList.add('hidden');
+  }
+};
+
+const createComments = () => {
   const fragment = document.createDocumentFragment();
 
-  comments.forEach((comment) => {
+  localComments.splice(0, COMMENTS_PORTION).forEach((comment) => {
     const createCommentTag = createSocialCommentTag(comment);
     fragment.append(createCommentTag);
+    renderedCommentsCount++;
   });
   commentsContainer.append(fragment);
+
+  renderLoader();
+  renderStatistics();
 };
+
+newCommentsLoader.addEventListener('click', createComments);
 
 const renderBigPicture = (photo) => {
   imageTag.src = photo.url;
   bigPictureTag.querySelector('.likes-count').textContent = photo.likes;
-  socialCommentsCount.textContent = photo.length;
+  bigPictureTag.querySelector('.social__caption').textContent = photo.description;
+  imageTag.alt = photo.description;
   commentsContainer.innerHTML = '';
-
+  localComments.length = 0;
+  localComments.push(...photo.comments.slice());
+  total = localComments.length;
+  renderedCommentsCount = 0;
   createComments();
-  bigPictureTag.querySelector('.social__caption').alt = photo.description;
 };
 
 const openModal = (data) => {
   bigPictureTag.classList.remove('hidden');
-  socialCommentsCount.classList.add('hidden');
-  newCommentsLoader.classList.add('hidden');
   document.body.classList.add('modal-open');
   renderBigPicture(data);
   document.addEventListener('keydown', onPopupEscKeydown);
@@ -48,10 +71,10 @@ const openModal = (data) => {
 
 closeButtonTag.addEventListener('click', (evt) => {
   evt.preventDefault();
-  bigPictureTag.classList.add('hidden');
+  closeModal();
 });
 
-const closeModal = () => {
+function closeModal() {
   bigPictureTag.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
