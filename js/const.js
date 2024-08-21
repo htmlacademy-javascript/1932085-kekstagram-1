26088a603fa3d01856fa280eadfc39afc12b1a02
Irina Vinogorskaya <bigshprot@yahoo.com>
2024-08-21 11:57:52 +0300

    дз 9 раздел часть 2

--- js/const.js
+++ js/const.js
@@ -2,6 +2,7 @@ export const MAX_DESCRIPTION_LENGTH = 140;
 export const MAX_HASHTAGS = 5;
 export const SPACE = /\s+/g;
 export const HASHTAG_FORMULA = /^#[a-zа-яё0-9]{1,19}$/i;
+
 export const Scale = {
   MAX: 100,
   MIN: 25,
